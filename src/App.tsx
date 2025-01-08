bae0c0a9657b2f4b6b073740f56c9a0da963af5f
LuizWillner <luizwillner@gmail.com>
2025-01-08 20:19:25 -0300

    Propriedade adicionada ao componente

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { Post } from "./components/Post";
 function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        autor="Luiz Willner"
+        conteudo="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro odit illum, suscipit, rerum alias amet accusamus id ratione dolore delectus ad animi harum dolorum at tempore accusantium dolores consectetur asperiores?"
+      />
+      <Post
+        autor="João Silva"
+        conteudo="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro odit illum, suscipit, rerum alias amet accusamus id ratione dolore delectus ad animi harum dolorum at tempore accusantium dolores consectetur asperiores?"
+      />
     </div>
   );
 }
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,5 +1,15 @@
-export function Post() {
-  return <p>Post</p>;
+// props : {autor: "Luiz Willner", conteudo: "Lorem ipsum, dolor sit amet consectetur..."}
+
+export function Post(props) {
+  console.log(props);
+  return (
+    <div>
+      <strong>{props.autor}</strong>
+      <p>{props.conteudo}</p>
+    </div>
+  );
 }
 
+// forma padrão de exportar um componente.
+// Desvantagem: a mudança de nome do componente no arquivo Post.tsx não é refletida automaticamente no arquivo App.tsx
 // export default Post;

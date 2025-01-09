be6abaa92adb36673bfd22795617f88816c34e0a
LuizWillner <luizwillner@gmail.com>
2025-01-08 21:56:02 -0300

    Header com imagem

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 // import "../styles/Header.css"; // tem que importar assim quando usa arquivo .css
 import styles from "../styles/Header.module.css"; // tem que importar assim quando usa arquivo .module.css
+import ignitelogo from "../assets/ignite-logo.svg";
 
 export function Header() {
   // "header" é uma classe do arquivo Header.module.css
   return (
     <header className={styles.header}>
-      <strong>Mist Forum</strong>
+      <img src={ignitelogo} alt="Logotipo do ignite" />
     </header>
   );
 }

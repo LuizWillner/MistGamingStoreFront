047f8282f0acc7aa40f33095593251524d104fc0
LuizWillner <luizwillner@gmail.com>
2025-01-29 22:39:36 -0300

    FEAT: Botões de alterar e deletar jogo pelo card do jogo

--- src/pages/DetalhesGame.tsx
+++ src/pages/DetalhesGame.tsx
@@ -7,24 +7,37 @@ import {
     faCartPlus,
     faCheck
   } from "@fortawesome/free-solid-svg-icons";
-import { Card, Col, Row } from "react-bootstrap";
+import { Card, Col, Row, Button } from "react-bootstrap";
 import { Game } from "../interfaces/game";
 import { CartItemPost } from "../interfaces/cartItem";
 import { useAdicionarItemCarrinho } from "../hooks/useAdicionarItemCarrinho";
 import "../styles/GameCard.css";
 import { useUsuarioStore } from "../store/useUsuarioStore";
+import { useGameStore } from "../store/useGameStore";
+import { useRemoverGame } from "../hooks/useRemoverGame";
 
 
 export const DetalhesGame = () => {
     const location = useLocation();
     const navigate = useNavigate();
     const [addedToCart, setAddedToCart] = useState(false);
+    const [isDisabled, setDisabled] = useState(false);
     const usuarioLogado = useUsuarioStore((s) => s.usuarioLogado);
+    const setGameSelecionado = useGameStore(s => s.setGameSelecionado);
 
     var game = location.state.data;
-    console.log(game.trailer);
 
-    const { mutate: cadastrarCartItem, error: errorCadastrarCartItem } = useAdicionarItemCarrinho();
+    const { 
+        mutate: cadastrarCartItem, 
+        error: errorCadastrarCartItem 
+    } = useAdicionarItemCarrinho();
+
+    const {
+        data: gameRemovido,
+        mutate: removerGame,
+        isLoading: removendo,
+        error: erroRemocao,
+    } = useRemoverGame();
 
     const tratarAdicionarGameAoCarrinho = (game: Game) => {
       if (!usuarioLogado) {
@@ -41,16 +54,28 @@ export const DetalhesGame = () => {
       setAddedToCart(true);
     };
 
+    const tratarRemocaoGame = (gameId: number) => {
+        removerGame(gameId);
+        setDisabled(true);
+    }
+
+    const tratarGameSelecionado = (game: Game) => {
+        setGameSelecionado(game);
+        navigate("/painel-admin")
+    }
+
     const handleVoltar = () => {
         navigate(-1);
     };
 
-  // Extrair e formatar a data de lançamento
-  const releaseDate = new Date(game.releaseDate);
-  const releaseDay = String(releaseDate.getDate()).padStart(2, '0');
-  const releaseMonth = String(releaseDate.getMonth() + 1).padStart(2, '0'); // getMonth() retorna 0-11, então adicionamos 1
-  const releaseYear = releaseDate.getFullYear();
-  const formattedReleaseDate = `${releaseDay}/${releaseMonth}/${releaseYear}`;
+    if (erroRemocao) throw erroRemocao;
+
+    // Extrair e formatar a data de lançamento
+    const releaseDate = new Date(game.releaseDate);
+    const releaseDay = String(releaseDate.getDate()).padStart(2, '0');
+    const releaseMonth = String(releaseDate.getMonth() + 1).padStart(2, '0'); // getMonth() retorna 0-11, então adicionamos 1
+    const releaseYear = releaseDate.getFullYear();
+    const formattedReleaseDate = `${releaseDay}/${releaseMonth}/${releaseYear}`;
 
     return (
         <>
@@ -58,10 +83,29 @@ export const DetalhesGame = () => {
                 <button onClick={handleVoltar} className="btn btn-azul-nub mb-3">
                     <FontAwesomeIcon icon={faArrowAltCircleLeft} /> Voltar
                 </button>
+                {gameRemovido && <p className="m-3 text-removido-com-sucesso">Jogo removido da loja com sucesso!</p>}
                 <Card className="card-detalhe " style={{backgroundColor: "#566878"}}>
-                    <h1 className="mb-4 w"><strong>{game.name}</strong></h1>
-                    <Row className="mb-3">
-                        <Col md={6}>
+                    <div className="d-flex align-items-start mb-4">
+                        <h1 className="w mr-3"><strong>{game.name}</strong></h1>
+                        <Button
+                            type="button"
+                            className="ml-3 btn btn-sm btn-danger align-self-center"
+                            disabled={isDisabled}
+                            onClick={() => tratarRemocaoGame(game.gameId!)}
+                            style={{marginLeft: 'auto'}}
+                        >
+                            Deletar
+                        </Button>
+                        <Button
+                            className="btn btn-sm btn-info align-self-center"
+                            disabled={isDisabled} onClick={() => tratarGameSelecionado(game)}
+                            style={{marginLeft: '10px'}}
+                        >
+                            Alterar
+                        </Button>
+                    </div>
+                    <Row className="mb-1">
+                        <Col md={6} className="d-flex flex-column align-items-start mb-4">
                             <img src={game.image} alt={game.name} className="card-image-filme" style={{width: '250px'}} />
                         </Col>
                         <Col md={6}>
@@ -84,7 +128,7 @@ export const DetalhesGame = () => {
                                     maximumFractionDigits: 2,
                                     useGrouping: true
                                 })}
-                                    <span className="card-text discount-badge">
+                                    <span className="card-text discount-badge-detalhes">
                                         <strong>
                                         R${(game.price * (1-game.discount)).toLocaleString("pt-BR", {
                                             minimumFractionDigits: 2,
@@ -93,7 +137,7 @@ export const DetalhesGame = () => {
                                         })}
                                         </strong>
                                     </span>
-                                    <span className="card-text discount-value">
+                                    <span className="card-text discount-value-detalhes">
                                         {(game.discount*100).toLocaleString("pt-BR", {
                                             maximumFractionDigits: 2,
                                             minimumFractionDigits: 2,
@@ -116,6 +160,7 @@ export const DetalhesGame = () => {
                                 <button 
                                   className={`btn ${addedToCart ? 'btn-success' : 'btn-primary'}`} 
                                   onClick={() => tratarAdicionarGameAoCarrinho(game)}
+                                  disabled={isDisabled}
                                 >
                                   {addedToCart ? <FontAwesomeIcon icon={faCheck} /> : <FontAwesomeIcon icon={faCartPlus} />}
                                 </button>
